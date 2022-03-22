22e7c756ec52f0f95126c798ffb8a75bb4f04edc
denasgt123 <rohmantesting83@gmail.com>
2022-03-22 19:42:58 +0700

    final last

--- lib/done_tourism_place_list.dart
+++ lib/done_tourism_place_list.dart
@@ -11,7 +11,7 @@ class DoneTourismList extends StatelessWidget {
     final List<TourismPlace> doneTourismPlaceList =
         Provider.of<DoneTourismProvider>(context, listen: false)
             .doneTourismPlaceList;
-    
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Wisata Telah Dikunjungi'),
@@ -22,12 +22,32 @@ class DoneTourismList extends StatelessWidget {
           return Card(
             color: Colors.white60,
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text(
-                  place.name,
-                  style: const TextStyle(fontSize: 16.0),
+                Expanded(
+                  flex: 1,
+                  child: Image.asset(place.imageAsset),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          place.name,
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(place.location),
+                      ],
+                    ),
+                  ),
                 ),
                 const Icon(Icons.done_outline),
               ],
